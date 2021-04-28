7174d749318ce7b5b12f6a5eafee8f0e6f785868
root <root@DESKTOP-F8OPL9O.localdomain>
2021-04-28 13:18:07 -0400

    Added templating, index page

--- app.py
+++ app.py
@@ -1,44 +1,44 @@
 from flask import Flask, render_template, json
 import os
-import database.db_connector as db
+#import database.db_connector as db
 
 app = Flask(__name__)
-db_connection = db.connect_to_database()
+#db_connection = db.connect_to_database()
 
 @app.route('/index')
-def root():
+def index():
         return render_template("index.j2")
 
 @app.route('/')
 def root():
-        return render_template("main.j2")
+        return render_template("main.j2", my_string="Test string", my_list=[0,1,2,3,4,5])
 
 @app.route('/product_search')
-def root():
+def product_search():
         return render_template("product_search.j2")
 
 @app.route('/view_product_customer')
-def root():
+def view_product_customer():
         return render_template("view_product_customer.j2")
 
 @app.route('/view_cart')
-def root():
+def view_cart():
         return render_template("view_cart.j2")
 
 @app.route('/supplier_search')
-def root():
+def supplier_search():
         return render_template("supplier_search.j2")
 
 @app.route('/view_inventory')
-def root():
+def view_inventory():
         return render_template("view_inventory.j2")
 
-@app.route('/view_prduct_admin')
-def root():
+@app.route('/view_product_admin')
+def view_prduct_admin():
         return render_template("view_prduct_admin.j2")
 
 @app.route('/resupply_order')
-def root():
+def resupply_order():
         return render_template("resupply_order.j2")
 
 
